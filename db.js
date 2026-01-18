dd31dc00cf6fac69ddec0ef0486c697181439141
rathore077 <rahulrathorelakhau@gmail.com>
2026-01-18 21:36:03 +0530

    deployement

--- db.js
+++ db.js
@@ -1,15 +1,30 @@
-const mysql= require("mysql2");
+// const mysql= require("mysql2");
+
+// const db = mysql.createConnection({
+//     host:"localhost",
+//     user:"root",
+//     password:"",
+//     database:"online_exam"
+// });
+
+// db.connect(err=>{
+//     if (err) throw err;
+//     console.log("MySQL Connected");
+// });
+
+// module.exports =db;
+const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"",
-    database:"online_exam"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME
 });
 
-db.connect(err=>{
-    if (err) throw err;
-    console.log("MySQL Connected");
+db.connect(err => {
+  if (err) console.error("DB error:", err);
+  else console.log("MySQL Connected");
 });
 
-module.exports =db;
+module.exports = db;
--- server.js
+++ server.js
@@ -124,6 +124,11 @@ app.get("/exams", (req, res) => {
 });
 
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+// app.listen(3000, () => {
+//   console.log("Server running on port 3000");
+// });
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });

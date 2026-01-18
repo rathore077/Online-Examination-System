b65f549f47ffd529ad25f4264a9d10f4b57675ac
rathore077 <rahulrathorelakhau@gmail.com>
2026-01-18 21:32:34 +0530

    Update project.js

--- project.js
+++ project.js
@@ -1,12 +1,1 @@
-let express=require("express")
-let app=express();
 
-app.use(express.json())
-
-app.get("/student-read",(req,res)=>{
-    res.send("student view api")
-})
-app.post("/student-insert",(req,res)=>{
-    res.send("student insert api")
-})
-app.listen("8000")
